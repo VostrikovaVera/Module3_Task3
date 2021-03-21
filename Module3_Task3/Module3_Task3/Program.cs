5e06d90b6efd0b223702fb0d8f7dcb1cf218495b
Vira Vostrikova <vvostrikova@csod.com>
2021-03-21 13:42:08 +0200

    Initial commit

---
+++ Module3_Task3/Module3_Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Module3_Task3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // call method Show (result of delegate for method Class2.Result())
+        }
+
+        private void Show(bool isTrue)
+        {
+            Console.WriteLine(isTrue);
+        }
+    }
+}
